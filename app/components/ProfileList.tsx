f10803c6377c7e9d85cdb918339b60ac585e058d
Gary Aleño <cjbonillo4@gmail.com>
2026-09-13 18:23:42 +0800

    Implement ProfileList component: define Profile type and initialize component structure

---
+++ app/components/ProfileList.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+type Profile = {
+    name: string;
+    age: number;
+    course: string;
+};
+
+export default function ProfileList() {
+    
+}
