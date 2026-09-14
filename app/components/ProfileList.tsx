82ed06b8bae46a865d6de5e4f8f41ce7f17cf4d9
Gary Aleño <cjbonillo4@gmail.com>
2026-09-14 18:40:51 +0800

    Implement ProfileList component: initialize profiles state and render profile details

--- app/components/ProfileList.tsx
+++ app/components/ProfileList.tsx
@@ -9,5 +9,27 @@ type Profile = {
 };
 
 export default function ProfileList() {
-    
+    const [profiles, setProfiles] = useState<Profile[]>([
+        {
+        name: "Olegario",
+        age: 29,
+        course: "BSIT",
+    },
+    {
+        name: "Juan",
+        age: 25,
+        course: "BSIT",
+    },
+    ]);
+    return (
+        <div>
+            {profiles.map((profile)=> (
+                <div key={profile.name}>
+                <p>{profile.name}</p>
+                <p>{profile.age}</p>
+                <p>{profile.course}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
